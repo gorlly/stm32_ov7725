8c958dba265fabc4dd8b9b25125daabc8a9748f7
gorlly <g-o-t-o@163.com>
2017-04-02 20:40:18 +0800

    second commit

--- USER/dma.h
+++ USER/dma.h
@@ -3,10 +3,13 @@
 
 #include "../STM32F10x_FWLib/inc/stm32f10x_dma.h"
 #include "system_stm32f10x.h"
-
-void MYDMA_Config(DMA_Channel_TypeDef* DMA_CHx,u32 cpar,u32 cmar,u16 cndtr);
 void MYDMA_Enable(DMA_Channel_TypeDef*DMA_CHx);
 
+
+
+void MYDMA_Config(DMA_Channel_TypeDef* DMA_CHx,u32 cpar,u32 cmar,u16 cndtr);
+
+//****************end of file **********************//
 
 #endif 
 
